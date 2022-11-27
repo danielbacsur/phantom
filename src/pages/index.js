feadab815993a27fd77c56d10e8137b2b4421ee5
danielbacsur <contact@danielbacsur.com>
2022-11-28 00:41:59 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,12 +16,12 @@ export default function App() {
   return (
     <div className="w-screen h-screen bg-white text-white font-karla">
         <div className="w-full h-32 shadow-[0px_22px_70px_4px_rgba(0,0,0,0.1)] bg-black/10 z-50 fixed top-0">
-    <div className="h-full container mx-auto flex items-center justify-between p-8">
-      <Link className="font-tostada text-2xl" href="/">
+    <div className="h-full container mx-auto flex items-center justify-center md:justify-between p-8">
+      <Link className="font-tostada text-2xl hidden md:block" href="/">
         phantom
       </Link>
       <div className="flex space-x-8">
-        <Link className="text-sm hidden md:block font-bold" href="/">
+        <Link className="text-sm font-bold" href="/">
           Home
         </Link>
         <Link className="text-sm font-bold" href="/about">
