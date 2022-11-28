67a63856ae560ffcea205ee0efaaf27f1c33fbae
danielbacsur <contact@danielbacsur.com>
2022-11-28 09:33:04 +0100

    Update team.js

--- src/static/team.js
+++ src/static/team.js
@@ -1,13 +1,13 @@
 const team = [
   {
     name: "Daniel Bacsur",
-    image: "dani.jpg",
+    image: "/dani.jpg",
     role: "Hardware and software developer",
     description:
       "Hi! I'm Dani. Lover of electronic design, self-taught programmer and winner of the Best Hardware award at last year's RoboCup Junior OnStage.",
   }, {
     name: "Erik Mikó",
-    image: "erik.jpg",
+    image: "/erik.jpg",
     role: "Embedded system developer",
     description:
       "Hi! I'm Erik. In recent years, I have been working with Linux systems and ethical hacking. In this project, I will be responsible for the os. system and security of the product.",
